7924d419af52ef565dc079bb98e148c83f39450a
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2019-05-24 15:04:23 -0500

    dia de implementacion

--- arduino/urls.py
+++ arduino/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
